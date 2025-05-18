c0bf3a621d3a128e7e840aae1d427ac400412619
hyunchul jung <devlibreta@hyunchului-Macmini.local>
2025-05-18 21:45:04 +0900

    add SecurityConfig class for h2 db

---
+++ src/main/java/com/dev/restfulspringapp/global/config/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.dev.restfulspringapp.global.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+        )
+        .csrf(AbstractHttpConfigurer::disable)
+        .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+        );
+        return http.build();
+    }
+}
