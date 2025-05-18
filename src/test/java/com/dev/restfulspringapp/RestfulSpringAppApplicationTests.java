a876ccf3bde1893f1984428cda782dd10c03d5ba
hyunchul jung <devlibreta@hyunchului-Macmini.local>
2025-05-18 21:14:01 +0900

    Initial commit

---
+++ src/test/java/com/dev/restfulspringapp/RestfulSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.restfulspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
