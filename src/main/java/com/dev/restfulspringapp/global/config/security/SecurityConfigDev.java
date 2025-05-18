084be4c2565cdfc6e3be877bf6c01ccb1845be23
hyunchul jung <devlibreta@hyunchului-Macmini.local>
2025-05-18 22:28:36 +0900

    split SecurityConfig class to dev, local

--- src/main/java/com/dev/restfulspringapp/global/config/security/SecurityConfigDev.java
+++ src/main/java/com/dev/restfulspringapp/global/config/security/SecurityConfigDev.java
@@ -2,13 +2,15 @@ package com.dev.restfulspringapp.global.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+@Profile("dev")
+public class SecurityConfigDev {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
--- src/main/java/com/dev/restfulspringapp/global/config/security/SecurityConfigLocal.java
+++ src/main/java/com/dev/restfulspringapp/global/config/security/SecurityConfigLocal.java
@@ -2,22 +2,23 @@ package com.dev.restfulspringapp.global.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+@Profile("local")
+public class SecurityConfigLocal {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/h2-console/**").permitAll()
-                .anyRequest().authenticated()
-        )
-        .csrf(AbstractHttpConfigurer::disable)
-        .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
-        );
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                );
         return http.build();
     }
 }
